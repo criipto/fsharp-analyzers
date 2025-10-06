7be1d968008d8d7a02feefe5376fa3de8797dcba
Frederik Krogsdal Jacobsen <frederik.krogsdal@criipto.com>
2025-10-06 15:45:55 +0200

    Add analyzer for ignoring results in do!.
    This pattern is dangerous because it makes it easy to write very subtle
    bugs that ignore error handling.

---
+++ tests/FSharp.Analyzers.Tests/data/doResultIgnore/positive/TaskResultIgnore.fs
@@ -0,0 +1,19 @@
+module M
+
+open FsToolkit.ErrorHandling
+
+let validateSubfunction (input : string) =
+    if (input <> "abc") then
+        TaskResult.error "Error"
+    else
+        TaskResult.ofResult (Result.Ok input)
+
+let validate (input : string) = taskResult {
+    return validateSubfunction input
+}
+
+let main : TaskResult<unit,string> = taskResult {
+    let a = "def"
+    do! validate a |> TaskResult.ignore
+    return ()
+}
